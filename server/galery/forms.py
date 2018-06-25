c74ceeb9c78187215338d02551d7fb15cdefa47e
Akos Goracz <akos.zoltan.goracz@gmail.com>
2018-06-25 07:52:04 +0200

    Removed tags, assets, forms and bower

--- server/galery/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class UploadForm(forms.Form):
-    images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True}))
-
-class DifferenceForm(forms.Form):
-    files = forms.CharField(widget=forms.Textarea)
